0bb5983d4fe657113f01905e1243a51ba3b9e7d6
AmBitSite <yfljvtymitgbnmrtif@yandex.ru>
2020-01-22 18:50:16 +0300

    22.01.2020

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@ let inputCurrencyQuantity = document.getElementById("change-currency-quantity")
 let changeWillGetText = document.getElementById("change-will-get-text");
 let inputCurrencyWillGet = document.getElementById("change-will-get-quantity");
 let btnExchangeNextStep =document.getElementById("exchange-next-step__btn");
-let pointerExchange = document.querySelector(".exchange-form-pointer__item")
+let pointerExchange = document.querySelector(".exchange-form-pointer__item");
+let exchangeBtn = document.getElementById("exchange-button");
+let exchangeButtonCancel = document.getElementById("exchange-button_cancel")
 
 function openWrap(element){
     for(let i = 1; i< element.children.length; i++){
@@ -28,15 +30,26 @@ currencyWant.addEventListener("click", ()=>{
     changeText(currencyWant, changeWillGetText)
 });
 
-inputCurrencyQuantity.addEventListener("keypress", ()=>{})
+inputCurrencyQuantity.addEventListener("keydown", (e)=>{
+    if(e.key!== "Backspace" || e.key!== "Delete")alert(inputCurrencyQuantity.value + e.key)
+    
+
+})
 // function searchLetters(elem){
 //     (elem.value.search()!=-1)
 // }
-btnExchangeNextStep.addEventListener("click", ()=>{nextFormSlide()})
+
+
 function nextFormSlide(){
-    btnExchangeNextStep.parentNode.classList.toggle("active")
-    btnExchangeNextStep.parentNode.nextElementSibling.classList.toggle("active");
+    let activeElem = document.querySelector(".active");
+    activeElem.classList.toggle("active");
+    activeElem.nextElementSibling.classList.toggle("active");
     pointerExchange.classList.toggle("exchange-form-pointer__item_active");
     pointerExchange.nextElementSibling.classList.toggle("exchange-form-pointer__item_active");
+    if(document.querySelector(".exchange-form__third-form").classList.contains("active")){
+        pointerExchange.parentNode.classList.toggle("d-none")
+    }
 }
-pointerExchange.addEventListener("click", ()=>{nextFormSlide()})
+// pointerExchange.addEventListener("click", ()=>{nextFormSlide()})
+exchangeBtn.addEventListener("click", ()=>{nextFormSlide()});
+btnExchangeNextStep.addEventListener("click", ()=>{nextFormSlide()});
